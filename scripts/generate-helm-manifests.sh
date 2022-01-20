ac06e1d6d215043eef5ee16e33bbc9509d43d268
Dennis Riemenschneider <38858747+300481@users.noreply.github.com>
2022-01-20 22:40:00 +0100

    add generate-helm-manifests.sh and values and generated manifests

---
+++ scripts/generate-helm-manifests.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+CONFIGFILE=helm-charts.yaml
+
+install_render() {
+    [[ -f /usr/local/bin/render ]] && return
+    local version=$(curl -s https://github.com/VirtusLab/render/releases/latest | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]')
+    wget -O render https://github.com/VirtusLab/render/releases/download/${version}/render-linux-amd64
+    sudo install render /usr/local/bin/render
+}
+
+repos() {
+    local repo_template='{{ range $repo, $values := .repos }}{{ $repo }} {{ end }}'
+    echo ${repo_template} | render -s --config ${CONFIGFILE}
+}
+
+
+charts() {
+    local repo=$1
+    local chart_template='{{ range $chart, $values := .repos.'${repo}'.charts }}{{ $chart }} {{ end }}'
+    echo ${chart_template} | render -s --config ${CONFIGFILE}
+}
+
+version() {
+    local repo=$1 ; shift ; local chart=$1
+    local version_template="{{ .repos.${repo}.charts.${chart}.version }}"
+    echo ${version_template} | render -s --config ${CONFIGFILE}
+}
+
+chart_name() {
+    local repo=$1 ; shift ; local chart=$1
+    local chart_name_template="{{ .repos.${repo}.charts.${chart}.name }}"
+    echo ${chart_name_template} | render -s --config ${CONFIGFILE}
+}
+
+repo_name() {
+    local repo=$1
+    local repo_name_template="{{ .repos.${repo}.name }}"
+    echo ${repo_name_template} | render -s --config ${CONFIGFILE}
+}
+
+repo_url() {
+    local repo=$1
+    local url_template="{{ .repos.${repo}.url }}"
+    echo ${url_template} | render -s --config ${CONFIGFILE}
+}
+
+install_render
+
+for repo in $(repos) ; do
+    repo_url=$(repo_url ${repo})
+    repo_name=$(repo_name ${repo})
+    helm repo add ${repo_name} ${repo_url}
+    helm repo update
+    for chart in $(charts ${repo}) ; do
+        chart_version=$(version ${repo} ${chart})
+        chart_name=$(chart_name ${repo} ${chart})
+        echo "${repo_name} | ${repo_url} | ${chart_name} | ${chart_version}"
+
+        valuesdir="chart-values/${repo_name}/${chart_name}"
+        manifestdir="chart-manifests/${repo_name}/${chart_name}"
+        [[ -d ${manifestdir} ]] || mkdir -p ${manifestdir}
+        [[ -d ${valuesdir} ]] || $(mkdir -p ${valuesdir} ; touch ${valuesdir}/default.yaml)
+
+        for valuesfile in ${valuesdir}/* ; do
+            manifestfile=${manifestdir}/$(basename ${valuesfile})
+            helm template ${chart_name} ${repo_name}/${chart_name} --version ${chart_version} --values ${valuesfile} > ${manifestfile}
+        done        
+    done
+done
