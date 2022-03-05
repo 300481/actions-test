1aae156abbf3c0efb939ae52b0810399ff1eeba9
Dennis Riemenschneider <38858747+300481@users.noreply.github.com>
2022-03-05 21:11:30 +0100

    refactor generate-list-of-cves.sh

--- artifacthub2github/generate-list-of-cves.sh
+++ artifacthub2github/generate-list-of-cves.sh
@@ -7,24 +7,24 @@
 : ${CVES_ROOT:=.}
 : ${TRIVY_TEMPLATE:=markdown.tpl}
 
+REPO_NAME=$(repo_name "${PAYLOAD}")
+PUBLISHER=$(publisher "${PAYLOAD}")
+CHART_NAME=$(chart_name "${PAYLOAD}")
+VERSION=$(version "${PAYLOAD}")
+REPO_URL=$(repo_url "${PAYLOAD}")
+
 install_render
 install_trivy
 
-for repo in $(repos) ; do
-    repo_name=$(repo_name ${repo})
-    for chart in $(charts ${repo}) ; do
-        chart_name=$(chart_name ${repo} ${chart})
-        manifestdir="${MANIFESTS_ROOT}/chart-manifests/${repo_name}/${chart_name}"
-        cvedir="${CVES_ROOT}/chart-cves/${repo_name}/${chart_name}"
-        [[ -d ${manifestdir} ]] || mkdir -p ${manifestdir}
-        [[ -d ${cvedir} ]] || mkdir -p ${cvedir}
-        for manifestfile in ${manifestdir}/*.yaml ; do
-            for image in $(grep -o 'image: .*$' ${manifestfile} | sed 's#image: ##g' | sed 's#"##g') ; do
-                echo ${image}
-                image_without_repo=${image##*/}
-                image_without_tag=${image_without_repo/:*/}
-                trivy image --format template --template "@${TRIVY_TEMPLATE}" --output ${cvedir}/${manifestfile##*/}.${image_without_tag}.cves.md --severity "MEDIUM,HIGH,CRITICAL" "${image}"
-            done
-        done        
+manifestdir="${MANIFESTS_ROOT}/chart-manifests/${REPO_NAME}/${CHART_NAME}"
+cvedir="${CVES_ROOT}/chart-cves/${REPO_NAME}/${CHART_NAME}"
+[[ -d ${manifestdir} ]] || mkdir -p ${manifestdir}
+[[ -d ${cvedir} ]] || mkdir -p ${cvedir}
+for manifestfile in ${manifestdir}/*.yaml ; do
+    for image in $(grep -o 'image: .*$' ${manifestfile} | sed 's#image: ##g' | sed 's#"##g') ; do
+        echo ${image}
+        image_without_repo=${image##*/}
+        image_without_tag=${image_without_repo/:*/}
+        trivy image --format template --template "@${TRIVY_TEMPLATE}" --output ${cvedir}/${manifestfile##*/}.${image_without_tag}.cves.md --severity "MEDIUM,HIGH,CRITICAL" "${image}"
     done
 done
