91b9cff21f52828c511646761491670c6fbb05c5
Dennis Riemenschneider <38858747+300481@users.noreply.github.com>
2022-03-05 20:36:29 +0100

    move function definition of generate-helm-manifests.sh to shared-functions.sh

--- artifacthub2github/shared-functions.sh
+++ artifacthub2github/shared-functions.sh
@@ -29,3 +29,48 @@ repo_url(){
     local REPOSITORY_NAME=$(repo_name "${1}")
     curl -s "https://artifacthub.io/api/v1/repositories/search?offset=0&limit=20&kind=0&user=${REPOSITORY_PUBLISHER}&org=${REPOSITORY_PUBLISHER}&name=${REPOSITORY_NAME}" | jq -r .[0].url
 }
+
+# install_render installs the render program
+install_render() {
+    [[ -f /usr/local/bin/render ]] && return
+    local version=$(curl -s https://github.com/VirtusLab/render/releases/latest | grep -oE 'v[0-9]+\.[0-9]+\.[0-9]')
+    cd /tmp
+    wget -O render https://github.com/VirtusLab/render/releases/download/${version}/render-linux-amd64
+    sudo install render /usr/local/bin/render
+    cd -
+}
+
+repos() {
+    local repo_template='{{ range $repo, $values := .repos }}{{ $repo }} {{ end }}'
+    echo ${repo_template} | render -s --config ${CONFIGFILE}
+}
+
+charts() {
+    local repo=$1
+    local chart_template='{{ range $chart, $values := .repos.'${repo}'.charts }}{{ $chart }} {{ end }}'
+    echo ${chart_template} | render -s --config ${CONFIGFILE}
+}
+
+version() {
+    local repo=$1 ; shift ; local chart=$1
+    local version_template="{{ .repos.${repo}.charts.${chart}.version }}"
+    echo ${version_template} | render -s --config ${CONFIGFILE}
+}
+
+chart_name() {
+    local repo=$1 ; shift ; local chart=$1
+    local chart_name_template="{{ .repos.${repo}.charts.${chart}.name }}"
+    echo ${chart_name_template} | render -s --config ${CONFIGFILE}
+}
+
+repo_name() {
+    local repo=$1
+    local repo_name_template="{{ .repos.${repo}.name }}"
+    echo ${repo_name_template} | render -s --config ${CONFIGFILE}
+}
+
+repo_url() {
+    local repo=$1
+    local url_template="{{ .repos.${repo}.url }}"
+    echo ${url_template} | render -s --config ${CONFIGFILE}
+}
