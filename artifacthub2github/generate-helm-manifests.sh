dbb0898c38a66a503f9c02bb65b03abd420e4fcc
Dennis Riemenschneider <38858747+300481@users.noreply.github.com>
2022-03-05 21:02:34 +0100

    refactor generate-helm-manifests.sh

--- artifacthub2github/generate-helm-manifests.sh
+++ artifacthub2github/generate-helm-manifests.sh
@@ -6,26 +6,25 @@
 : ${VALUES_ROOT:=.}
 : ${MANIFESTS_ROOT:=.}
 
+REPO_NAME=$(repo_name "${PAYLOAD}")
+PUBLISHER=$(publisher "${PAYLOAD}")
+CHART_NAME=$(chart_name "${PAYLOAD}")
+VERSION=$(version "${PAYLOAD}")
+REPO_URL=$(repo_url "${PAYLOAD}")
+
 install_render
 
-for repo in $(repos) ; do
-    repo_url=$(repo_url ${repo})
-    repo_name=$(repo_name ${repo})
-    helm repo add ${repo_name} ${repo_url}
-    helm repo update
-    for chart in $(charts ${repo}) ; do
-        chart_version=$(version ${repo} ${chart})
-        chart_name=$(chart_name ${repo} ${chart})
-        echo "${repo_name} | ${repo_url} | ${chart_name} | ${chart_version}"
+echo "${REPO_NAME} | ${REPO_URL} | ${CHART_NAME} | ${VERSION}"
 
-        valuesdir="${VALUES_ROOT}/chart-values/${repo_name}/${chart_name}"
-        manifestdir="${MANIFESTS_ROOT}/chart-manifests/${repo_name}/${chart_name}"
-        [[ -d ${manifestdir} ]] || mkdir -p ${manifestdir}
-        [[ -d ${valuesdir} ]] || $(mkdir -p ${valuesdir} ; touch ${valuesdir}/default.yaml)
+helm repo add ${REPO_NAME} ${REPO_URL}
+helm repo update
 
-        for valuesfile in ${valuesdir}/* ; do
-            manifestfile=${manifestdir}/$(basename ${valuesfile})
-            helm template ${chart_name} ${repo_name}/${chart_name} --version ${chart_version} --values ${valuesfile} > ${manifestfile}
-        done        
-    done
-done
+valuesdir="${VALUES_ROOT}/chart-values/${REPO_NAME}/${CHART_NAME}"
+manifestdir="${MANIFESTS_ROOT}/chart-manifests/${REPO_NAME}/${CHART_NAME}"
+[[ -d ${manifestdir} ]] || mkdir -p ${manifestdir}
+[[ -d ${valuesdir} ]] || $(mkdir -p ${valuesdir} ; touch ${valuesdir}/default.yaml)
+
+for valuesfile in ${valuesdir}/* ; do
+    manifestfile=${manifestdir}/$(basename ${valuesfile})
+    helm template ${CHART_NAME} ${REPO_NAME}/${CHART_NAME} --version ${VERSION} --values ${valuesfile} > ${manifestfile}
+done        
