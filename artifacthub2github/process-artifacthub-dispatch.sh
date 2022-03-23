9158c2411739276d4db51e4a1414838a49aaecea
Dennis Riemenschneider <38858747+300481@users.noreply.github.com>
2022-03-23 21:46:26 +0100

    move functionality of generate-helm-manifests to process-artifacthub-dispatch.sh

--- artifacthub2github/process-artifacthub-dispatch.sh
+++ artifacthub2github/process-artifacthub-dispatch.sh
@@ -41,4 +41,26 @@ update_chart_version(){
     yq eval '.repos.'${REPO}'.charts.'${CHART}'.name = "'${CHART_NAME}'"' -i ${CONFIGFILE}
 }
 
+generate_helm_manifests(){
+    REPO_NAME=$(repo_name "${PAYLOAD}")
+    CHART_NAME=$(chart_name "${PAYLOAD}")
+    VERSION=$(version "${PAYLOAD}")
+    REPO_URL=$(repo_url "${PAYLOAD}")
+
+    echo "${REPO_NAME} | ${REPO_URL} | ${CHART_NAME} | ${VERSION}"
+
+    helm repo add ${REPO_NAME} ${REPO_URL}
+    helm repo update
+
+    valuesdir="${VALUES_ROOT}/chart-values/${REPO_NAME}/${CHART_NAME}"
+    manifestdir="${MANIFESTS_ROOT}/chart-manifests/${REPO_NAME}/${CHART_NAME}"
+    [[ -d ${manifestdir} ]] || mkdir -p ${manifestdir}
+    [[ -d ${valuesdir} ]] || $(mkdir -p ${valuesdir} ; touch ${valuesdir}/default.yaml)
+
+    for valuesfile in ${valuesdir}/* ; do
+        manifestfile=${manifestdir}/$(basename ${valuesfile})
+        helm template ${CHART_NAME} ${REPO_NAME}/${CHART_NAME} --version ${VERSION} --values ${valuesfile} > ${manifestfile}
+    done
+}
+
 ${1}
