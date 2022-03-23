1e6c57de5adb13f3b3aab82add92eed1aadf9ba3
Dennis Riemenschneider <38858747+300481@users.noreply.github.com>
2022-03-23 21:53:27 +0100

    add/move functionality of generate-list-of-deprecated-api-versions.sh to process-artifacthub-dispatch.sh

--- artifacthub2github/process-artifacthub-dispatch.sh
+++ artifacthub2github/process-artifacthub-dispatch.sh
@@ -64,4 +64,18 @@ generate_helm_manifests(){
     done
 }
 
+generate_list_of_deprecated_api_versions(){
+    REPO_NAME=$(repo_name "${PAYLOAD}")
+    CHART_NAME=$(chart_name "${PAYLOAD}")
+
+    manifestdir="${MANIFESTS_ROOT}/chart-manifests/${REPO_NAME}/${CHART_NAME}"
+    deprecationsdir="${DEPRECATIONS_ROOT}/chart-deprecations/${REPO_NAME}/${CHART_NAME}"
+    [[ -d ${manifestdir} ]] || mkdir -p ${manifestdir}
+    [[ -d ${deprecationsdir} ]] || mkdir -p ${deprecationsdir}
+    for manifestfile in ${manifestdir}/* ; do
+        echo ${REPO_NAME} : ${CHART_NAME} : ${manifestfile}
+        pluto detect ${manifestfile} --output markdown --ignore-deprecations --ignore-removals > ${deprecationsdir}/${manifestfile##*/}.api-deprecations.md
+    done
+}
+
 ${1}
