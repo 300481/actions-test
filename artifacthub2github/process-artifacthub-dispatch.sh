5fcd27942f38f1fa65a87457fdbcd715822db36d
Dennis Riemenschneider <38858747+300481@users.noreply.github.com>
2022-03-23 21:58:16 +0100

    move/add functionality of generate-list-of-cves.sh to process-artifacthub-dispatch.sh

--- artifacthub2github/process-artifacthub-dispatch.sh
+++ artifacthub2github/process-artifacthub-dispatch.sh
@@ -80,4 +80,22 @@ generate_list_of_deprecated_api_versions(){
     done
 }
 
+generate_list_of_cves(){
+    REPO_NAME=$(repo_name "${PAYLOAD}")
+    CHART_NAME=$(chart_name "${PAYLOAD}")
+
+    manifestdir="${MANIFESTS_ROOT}/chart-manifests/${REPO_NAME}/${CHART_NAME}"
+    cvedir="${CVES_ROOT}/chart-cves/${REPO_NAME}/${CHART_NAME}"
+    [[ -d ${manifestdir} ]] || mkdir -p ${manifestdir}
+    [[ -d ${cvedir} ]] || mkdir -p ${cvedir}
+    for manifestfile in ${manifestdir}/*.yaml ; do
+        for image in $(grep -o 'image: .*$' ${manifestfile} | sed 's#image: ##g' | sed 's#"##g') ; do
+            echo ${image}
+            image_without_repo=${image##*/}
+            image_without_tag=${image_without_repo/:*/}
+            trivy image --format template --template "@${TRIVY_TEMPLATE}" --output ${cvedir}/${manifestfile##*/}.${image_without_tag}.cves.md --severity "MEDIUM,HIGH,CRITICAL" "${image}"
+        done
+    done
+}
+
 ${1}
